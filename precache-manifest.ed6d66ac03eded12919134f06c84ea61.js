e9c862446024f31e5d59b14f3278ee2c4ef3edf2
antuan159 <lanos159@ukr.net>
2019-12-25 21:20:53 +0200

    Updates

---
+++ precache-manifest.ed6d66ac03eded12919134f06c84ea61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba855139bfadaa170dd7",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.2be183e4.chunk.css"
+  },
+  {
+    "revision": "ba855139bfadaa170dd7",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.2a9409dc.chunk.js"
+  },
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  },
+  {
+    "revision": "7e48a50c039c34ed57cd",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.fc125280.chunk.js"
+  },
+  {
+    "revision": "6b780d29f373ef557e00d2792c4e267e",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  }
+];
